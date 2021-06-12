1cb2e2185a3cdf7a9b23dcd440a514b349bed70d
Quanzheng Long <longquanzheng@users.noreply.github.com>
2021-06-12 09:39:52 -0700

    improve comment

--- src/main/java/com/uber/cadence/samples/hello/HelloSignalAndResponse.java
+++ src/main/java/com/uber/cadence/samples/hello/HelloSignalAndResponse.java
@@ -50,9 +50,9 @@ import java.util.Map;
 import org.apache.commons.lang.RandomStringUtils;
 
 /**
- * Demonstrates asynchronous signalling of a workflow, and wait until it's applied and get a
+ * Demonstrates signalling a workflow, and wait until it's applied and get a
  * response. This should be much performant(lower latency) than using signal+query approach.
- * Requires a local instance of Cadence server to be running.
+ * Requires a Cadence server to be running.
  */
 @SuppressWarnings("ALL")
 public class HelloSignalAndResponse {
