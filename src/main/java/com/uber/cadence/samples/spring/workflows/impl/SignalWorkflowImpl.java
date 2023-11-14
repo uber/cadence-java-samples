830da3eb4f4b38ee0bbc2801461b092bd58cae8c
Chris Qin <yqin6@ext.uber.com>
2023-11-13 16:03:50 -0800

    Bug fix: make sure signal message only printed once by correctly manipulating the cancel signal

--- src/main/java/com/uber/cadence/samples/spring/workflows/impl/SignalWorkflowImpl.java
+++ src/main/java/com/uber/cadence/samples/spring/workflows/impl/SignalWorkflowImpl.java
@@ -8,26 +8,33 @@ import org.slf4j.Logger;
 public class SignalWorkflowImpl implements SignalWorkflow {
   private final Logger logger = Workflow.getLogger(SignalWorkflowImpl.class);
   private String name;
-  private String greetingMsg;
+  private String greetingMsg = "";
   private boolean cancel = false;
 
   @Override
   public void getGreeting(SampleMessage sampleMessage) {
     logger.info("executing SignalWorkflow::getGreeting");
     this.name = sampleMessage.GetMessage();
-    Workflow.await(
-        () -> {
-          if (cancel) {
-            logger.info("SignalWorkflow cancelled");
-          }
-          logger.info("greeting: " + this.greetingMsg);
-          return cancel;
-        });
-    logger.info("greeting:" + this.greetingMsg);
+    int count = 0;
+
+    while (!cancel) {
+      Workflow.await(() -> cancel || !this.greetingMsg.isEmpty());
+      if (!this.greetingMsg.isEmpty()) {
+        logger.info(++count + ": " + this.greetingMsg + "!");
+        this.greetingMsg = "";
+      }
+    }
+
+    logger.info("workflow canceled");
   }
 
   @Override
   public void waitForGreeting(String greeting) {
+    if (cancel) {
+      logger.info("signal workflow failed because it is already cancelled");
+      return;
+    }
+
     logger.info("received signal from SignalWorkflow:waitForName");
     this.greetingMsg = String.format("%s, %s!", greeting, this.name);
   }
